66d85e1c4c1e6ba1fb03d5353340b44edb96ae0a
ahra1221 <lizze011221@gmail.com>
2024-02-05 02:27:36 +0900

    [Setting] #1 - 프로젝트 생성

---
+++ UmbrellaFriend-iOS/UmbrellaFriend-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UmbrellaFriend-iOS
+//
+//  Created by 고아라 on 2/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
