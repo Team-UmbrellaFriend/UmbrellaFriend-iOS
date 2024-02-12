ddc9817a2cd2a84c77d9e50385ff25a6248c9c99
ahra1221 <lizze011221@gmail.com>
2024-02-12 15:27:49 +0900

    #3 add: 폰트 추가

--- UmbrellaFriend-iOS/UmbrellaFriend-iOS/Global/Literals/FontLiterals.swift
+++ UmbrellaFriend-iOS/UmbrellaFriend-iOS/Global/Literals/FontLiterals.swift
@@ -5,4 +5,15 @@
 //  Created by 고아라 on 2/5/24.
 //
 
-import Foundation
+import UIKit
+
+struct FontName {
+    static let SUITSemiBold = "SUIT-SemiBold"
+}
+
+extension UIFont {
+    
+    @nonobjc class func SUITSemiBold(size: CGFloat) -> UIFont {
+        return UIFont(name: FontName.SUITSemiBold, size: size)!
+    }
+}
