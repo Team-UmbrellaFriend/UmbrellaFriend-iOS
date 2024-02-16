788a73dc7fac4ca7bafe3fb2e9fa84b8ebc90cef
ahra1221 <lizze011221@gmail.com>
2024-02-16 17:08:28 +0900

    #7 feat: SplashViewController 구현

---
+++ UmbrellaFriend-iOS/UmbrellaFriend-iOS/Presentation/Auth/LogoScene/ViewController/LogoViewController.swift
@@ -0,0 +1,64 @@
+//
+//  LogoViewController.swift
+//  UmbrellaFriend-iOS
+//
+//  Created by 고아라 on 2/16/24.
+//
+
+import UIKit
+
+final class LogoViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - UI Components
+    
+    
+    // MARK: - Life Cycles
+    
+    override func loadView() {
+        super.loadView()
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        getAPI()
+        setUI()
+        setHierarchy()
+        setLayout()
+        setDelegate()
+    }
+}
+
+// MARK: - Extensions
+
+extension LogoViewController {
+
+    func setUI() {
+        
+    }
+    
+    func setHierarchy() {
+        
+    }
+    
+    func setLayout() {
+        
+    }
+    
+    func setDelegate() {
+        
+    }
+}
+
+// MARK: - Network
+
+extension LogoViewController {
+
+    func getAPI() {
+        
+    }
+}
