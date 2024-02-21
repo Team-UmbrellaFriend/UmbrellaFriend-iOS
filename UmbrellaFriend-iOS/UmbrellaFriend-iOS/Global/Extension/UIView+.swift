180aae1bff7e6d1a76d54b9e6e38d806dbd2374e
ahra1221 <lizze011221@gmail.com>
2024-02-21 18:24:22 +0900

    #9 feat: 뷰에 그라데이션 넣는 extension 구현

--- UmbrellaFriend-iOS/UmbrellaFriend-iOS/Global/Extension/UIView+.swift
+++ UmbrellaFriend-iOS/UmbrellaFriend-iOS/Global/Extension/UIView+.swift
@@ -12,4 +12,13 @@ extension UIView {
     func addSubviews(_ views: UIView...) {
         views.forEach { self.addSubview($0) }
     }
+    
+    func applyGradient() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = [UIColor.gradation1, UIColor.gradation2].map { $0.cgColor }
+        gradientLayer.startPoint = CGPoint(x: 0, y: 1)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 0)
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
 }
