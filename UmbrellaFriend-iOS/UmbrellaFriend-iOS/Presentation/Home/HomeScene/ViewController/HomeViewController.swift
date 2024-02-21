ebd0abfa778513ed5ef29802118a70dd3077dd43
ahra1221 <lizze011221@gmail.com>
2024-02-21 22:55:16 +0900

    #9 feat: 홈화면 tapGesture 구현

--- UmbrellaFriend-iOS/UmbrellaFriend-iOS/Presentation/Home/HomeScene/ViewController/HomeViewController.swift
+++ UmbrellaFriend-iOS/UmbrellaFriend-iOS/Presentation/Home/HomeScene/ViewController/HomeViewController.swift
@@ -28,6 +28,7 @@ final class HomeViewController: UIViewController {
         super.viewDidLoad()
         
         setUI()
+        setGesture()
     }
 }
 
@@ -37,5 +38,34 @@ extension HomeViewController {
 
     func setUI() {
         self.navigationController?.navigationBar.isHidden = true
+    }
+    
+    func setGesture() {
+        let rentTapGesture = UITapGestureRecognizer(target: self, action: #selector(rentTapped))
+        homeView.rentView.addGestureRecognizer(rentTapGesture)
+        
+        let returnTapGesture = UITapGestureRecognizer(target: self, action: #selector(returnTapped))
+        homeView.returnView.addGestureRecognizer(returnTapGesture)
+        
+        let mapTapGesture = UITapGestureRecognizer(target: self, action: #selector(mapTapped))
+        homeView.mapView.addGestureRecognizer(mapTapGesture)
+    }
+    
+    @objc
+    func rentTapped() {
+        let nav = UmbrellaRentViewController()
+        self.navigationController?.pushViewController(nav, animated: true)
+    }
+    
+    @objc
+    func returnTapped() {
+//        let nav = UmbrellaReturnViewController()
+//        self.navigationController?.pushViewController(nav, animated: true)
+    }
+    
+    @objc
+    func mapTapped() {
+//        let nav = UmbrellaMapViewController()
+//        self.navigationController?.pushViewController(nav, animated: true)
     }
 }
