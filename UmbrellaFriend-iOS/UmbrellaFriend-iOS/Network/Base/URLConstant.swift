d7bf20a1c4daa91b74d5c88efd9ec286477b6e4b
ahra1221 <lizze011221@gmail.com>
2024-02-25 01:32:08 +0900

    #11 setting: url 세팅

---
+++ UmbrellaFriend-iOS/UmbrellaFriend-iOS/Network/Base/URLConstant.swift
@@ -0,0 +1,39 @@
+//
+//  URLConstant.swift
+//  UmbrellaFriend-iOS
+//
+//  Created by 고아라 on 2/25/24.
+//
+
+import Foundation
+
+enum URLConstant {
+    
+    // MARK: - Base URL
+    
+    static let baseURL = Config.baseURL
+    
+    // MARK: - URL Path
+    
+    // home
+
+    static let home = "/home/"
+    
+    // mypage
+    
+    static let mypage = "/mypage/"
+    
+    // umbrella
+    
+    static let umbrellaAvailable = "/umbrella/available/"
+    static let umbrellaCheck = "/umbrella/{UmbrellaNumber}/check/"
+    static let umbrellaLend = "/umbrella/{UmbrellaNumber}/lend/"
+    static let umbrellaReturn = "/umbrella/return/"
+    
+    // users
+    
+    static let userSignup = "/users/signup/"
+    static let userLogin = "/users/login/"
+    static let userLogout = "/users/logout/"
+    static let userProfile = "/users/profile/{UserId}/"
+}
