aeb35cd1c8efcd72eefd162b4a2663b6c0ef29f4
ahra1221 <lizze011221@gmail.com>
2024-02-25 01:33:39 +0900

    #feat: HomeViewModel 구현

---
+++ UmbrellaFriend-iOS/UmbrellaFriend-iOS/Presentation/Home/HomeScene/ViewModel/HomeViewModel.swift
@@ -0,0 +1,51 @@
+//
+//  HomeViewModel.swift
+//  UmbrellaFriend-iOS
+//
+//  Created by 고아라 on 2/25/24.
+//
+
+import UIKit
+
+import RxSwift
+import RxCocoa
+import Moya
+
+protocol HomeViewModelInputs {
+    
+}
+
+protocol HomeViewModelOutputs {
+    
+    var homeData: BehaviorRelay<HomeDto> { get }
+}
+
+protocol HomeViewModelType {
+    
+    var inputs: HomeViewModelInputs { get }
+    var outputs: HomeViewModelOutputs { get }
+}
+
+final class HomeViewModel: HomeViewModelInputs, HomeViewModelOutputs, HomeViewModelType {
+    
+    var inputs: HomeViewModelInputs { return self }
+    var outputs: HomeViewModelOutputs { return self }
+ 
+    var homeData: BehaviorRelay<HomeDto> = BehaviorRelay<HomeDto>(value: HomeDto.homeDtoInitValue())
+    
+    init() {
+        self.getHomeDto()
+    }
+}
+
+extension HomeViewModel {
+    
+    func getHomeDto() {
+        HomeAPI.shared.getHome { [weak self] response in
+            guard let status = response?.status else { return }
+            guard self != nil else { return }
+            guard let data = response?.data else { return }
+            self?.homeData.accept(data)
+        }
+    }
+}
