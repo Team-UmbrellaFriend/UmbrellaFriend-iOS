691a94f826c978b3a12fa6912a8261f0263db2e5
ahra1221 <lizze011221@gmail.com>
2024-04-17 16:36:40 +0900

    #29 feat: MypageVC에 로딩뷰 적용

--- UmbrellaFriend-iOS/UmbrellaFriend-iOS/Presentation/Mypage/MyPageScene/ViewController/MypageViewController.swift
+++ UmbrellaFriend-iOS/UmbrellaFriend-iOS/Presentation/Mypage/MyPageScene/ViewController/MypageViewController.swift
@@ -76,6 +76,13 @@ extension MypageViewController {
                 self.mypageLogoutSubject.onNext(())
             })
             .disposed(by: disposeBag)
+        
+        self.rx.viewWillAppear
+            .subscribe(onNext: { [weak self] _ in
+                guard let self = self else { return }
+                LoadingView.shared.show(self.view)
+            })
+            .disposed(by: disposeBag)
     }
 
     func bindViewModel() {
@@ -91,6 +98,9 @@ extension MypageViewController {
             .asDriver(onErrorJustReturn: MypageEntity.mypageEntityInitValue())
             .drive(with: self, onNext: { owner, mypage in
                 owner.mypageView.configureView(model: mypage)
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.3) {
+                    LoadingView.shared.hide()
+                }
             })
             .disposed(by: disposeBag)
         
