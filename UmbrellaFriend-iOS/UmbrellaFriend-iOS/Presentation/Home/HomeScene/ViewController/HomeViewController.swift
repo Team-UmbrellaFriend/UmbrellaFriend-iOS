8073df363f0635ee5b7c6f836eda9db2335409b3
ahra1221 <lizze011221@gmail.com>
2024-04-19 03:27:48 +0900

    #29 fix: 로딩뷰 수정

--- UmbrellaFriend-iOS/UmbrellaFriend-iOS/Presentation/Home/HomeScene/ViewController/HomeViewController.swift
+++ UmbrellaFriend-iOS/UmbrellaFriend-iOS/Presentation/Home/HomeScene/ViewController/HomeViewController.swift
@@ -112,7 +112,7 @@ extension HomeViewController {
             .asDriver(onErrorJustReturn: HomeEntity.homeDtoInitValue())
             .drive(with: self, onNext: { owner, home in
                 owner.homeView.configureHomeView(home)
-                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.8) {
                     LoadingView.shared.hide()
                 }
             })
