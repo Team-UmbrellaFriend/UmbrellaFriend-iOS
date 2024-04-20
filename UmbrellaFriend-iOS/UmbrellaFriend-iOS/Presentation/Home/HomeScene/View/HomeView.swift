bca1ba5fdb4a87ffd988601bbb1e58ba359f4ee1
ahra1221 <lizze011221@gmail.com>
2024-04-21 01:59:04 +0900

    #33 fix: 연장 로직 처리 수정

--- UmbrellaFriend-iOS/UmbrellaFriend-iOS/Presentation/Home/HomeScene/View/HomeView.swift
+++ UmbrellaFriend-iOS/UmbrellaFriend-iOS/Presentation/Home/HomeScene/View/HomeView.swift
@@ -435,7 +435,7 @@ extension HomeView {
     
     func configureHomeAlertView(_ message: String) {
         homeAlertView.changedSubtitle = message
-        homeAlertView.alertTitleLabel.text = message.contains("성공") ? "대여 연장 완료" : "대여 연장 실패"
+        homeAlertView.alertTitleLabel.text = message.contains("자동으로 3일") ? "대여 연장 완료" : "대여 연장 실패"
         homeAlertView.alertIcon.image = message.contains("성공") ? UIImage(resource: .icAlertCheck) : UIImage(resource: .icAlertFail)
     }
 }
