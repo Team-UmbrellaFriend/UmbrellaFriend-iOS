59343a5039c999cc368c9e73de8684c4692bd0e9
ahra1221 <lizze011221@gmail.com>
2024-04-29 21:38:00 +0900

    #35 feat: 업데이트 로직 구현

---
+++ UmbrellaFriend-iOS/UmbrellaFriend-iOS/Data/DTO/Spalsh/VersionDto.swift
@@ -0,0 +1,13 @@
+//
+//  VersionDto.swift
+//  UmbrellaFriend-iOS
+//
+//  Created by 고아라 on 4/29/24.
+//
+
+import Foundation
+
+struct VersionDto {
+    let recommendVersion: String
+    let forceVeresion: String
+}
--- UmbrellaFriend-iOS/UmbrellaFriend-iOS/Presentation/Splash/ViewController/SplashViewController.swift
+++ UmbrellaFriend-iOS/UmbrellaFriend-iOS/Presentation/Splash/ViewController/SplashViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
+import RxSwift
+import RxCocoa
+
 final class SplashViewController: UIViewController {
     
     // MARK: - UI Components
     
     private let splashView = SplashView()
     
+    private let viewModel: SplashViewModel
+    private let disposeBag = DisposeBag()
+    private let myVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+    private let appStoreURL = "itms-apps://itunes.apple.com/app/id6479554833"
+    
     // MARK: - Life Cycles
+    
+    init(viewModel: SplashViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func loadView() {
         
@@ -24,9 +41,8 @@ final class SplashViewController: UIViewController {
         super.viewDidLoad()
         
         setUI()
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.2) {
-            self.showNextPage()
-        }
+        bindUI()
+        bindViewModel()
     }
 }
 
@@ -37,6 +53,48 @@ extension SplashViewController {
     func setUI() {
         self.navigationController?.navigationBar.isHidden = true
     }
+    
+    func bindUI() {
+        splashView.forceUpdateAlert.alertCheckButton.rx.tap
+            .subscribe(onNext: { _ in
+                self.openAppStore(urlStr: self.appStoreURL)
+            })
+            .disposed(by: disposeBag)
+        
+        splashView.recommendOkButton.rx.tap
+            .subscribe(onNext: { _ in
+                self.openAppStore(urlStr: self.appStoreURL)
+            })
+            .disposed(by: disposeBag)
+        
+        splashView.recommendCancelButton.rx.tap
+            .subscribe(onNext: { _ in
+                self.showNextPage()
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    func bindViewModel() {
+        let input = SplashViewModel.Input(
+            viewWillAppearEvent: self.rx.viewWillAppear.asObservable()
+        )
+        let output = self.viewModel.transform(from: input, disposeBag: self.disposeBag)
+        
+        output.versionData
+            .map { entity in
+                return VersionDto(recommendVersion: entity.iosVersion.appVersion,
+                                  forceVeresion: entity.iosVersion.forceUpdateVersion)
+            }
+            .subscribe(onNext: { dto in
+                UserManager.shared.updateStoreVersion(dto.recommendVersion)
+                if !self.splashView.bindUpdateAlert(myVersion: self.myVersion ?? "", version: dto) {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                        self.showNextPage()
+                    }
+                }
+            })
+            .disposed(by: disposeBag)
+    }
 
     func showNextPage() {
         if UserManager.shared.hasToken {
@@ -46,6 +104,14 @@ extension SplashViewController {
         } else {
             let nav = LogoViewController()
             self.navigationController?.pushViewController(nav, animated: true)
+        }
+    }
+    
+    func openAppStore(urlStr: String) {
+        guard let url = URL(string: urlStr) else { return }
+        
+        if UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
         }
     }
 }
