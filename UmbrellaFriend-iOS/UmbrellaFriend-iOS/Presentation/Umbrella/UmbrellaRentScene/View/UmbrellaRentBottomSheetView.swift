8867f9930aaa3cef1948c9d7d71fecd28c37b5ad
ahra1221 <lizze011221@gmail.com>
2024-05-01 03:32:37 +0900

    #37 chore: 대여 alert 로직 수정

--- UmbrellaFriend-iOS/UmbrellaFriend-iOS/Presentation/Umbrella/UmbrellaRentScene/View/UmbrellaRentBottomSheetView.swift
+++ UmbrellaFriend-iOS/UmbrellaFriend-iOS/Presentation/Umbrella/UmbrellaRentScene/View/UmbrellaRentBottomSheetView.swift
@@ -98,7 +98,7 @@ final class UmbrellaRentBottomSheetView: UIView {
     }()
     
     private let rentIcon = UIImageView(image: UIImage(resource: .icBigUmbrella))
-    let rentAlertView = CustomAlertView(type: .notice, title: "잠시만요!", subTitle: "선택한 장소와 일치하지 않아요.\n다시 인증해주세요.")
+    let rentAlertView = CustomAlertView(type: .notice, title: "잠시만요!", subTitle: "")
     
     // MARK: - Life Cycles
     
@@ -211,16 +211,7 @@ extension UmbrellaRentBottomSheetView {
     }
     
     func configureAlertView(message: String) -> Bool {
-        if message.contains("대여했습니다") {
-            rentAlertView.alertTitleLabel.text = "대여완료!"
-            rentAlertView.alertTitleLabel.textColor = .mainBlue
-            rentAlertView.changedSubtitle = "\(message)."
-            return true
-        } else {
-            rentAlertView.alertTitleLabel.text = "잠깐만요!"
-            rentAlertView.alertTitleLabel.textColor = .subOrange
-            rentAlertView.changedSubtitle = message
-            return false
-        }
+        rentAlertView.changedSubtitle = message
+        return message.contains("대여했습니다")
     }
 }
